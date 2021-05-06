74fee32ef8e7bcf43058c1b4714d48d16f8ee84a
Babraham1995 <b.abrhm@hotmail.co.uk>
2021-05-06 18:38:13 +0100

    initial commit

---
+++ server/src/resolvers/user.ts
@@ -0,0 +1,136 @@
+import { User } from "../entities/User";
+import { MyContext } from "src/types";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
+import argon2 from 'argon2';
+import {EntityManager} from '@mikro-orm/postgresql';
+
+@InputType()
+class UsernamePasswordInput{
+@Field()
+username: string
+@Field()
+password: string
+}
+
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
+    @Field()
+    message: string;
+}
+
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], {nullable: true})
+    errors?: FieldError[]
+
+    @Field(() => User, {nullable: true})
+    user?: User
+}
+
+@Resolver()
+export class UserResolver{
+    @Query(() => User, {nullable: true})
+    me(
+        @Ctx() { req, em }: MyContext 
+    ){
+        if (!req.session.userId) {
+            return null
+        }
+        console.log("hi" + req.session.userId);
+        const user = em.findOne(User, {id: req.session.userId});
+        return user;
+    }
+
+
+    @Mutation(()=> UserResponse)
+    async register(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() {em, req}: MyContext
+        ): Promise<UserResponse> {
+            if( options.username.length<=3){
+            return {
+                errors: [{
+                    field: 'username',
+                    message: "length must be greater than 3"
+                }],
+            };
+            }
+            if( options.password.length<=3){
+                return {
+                    errors: [{
+                        field: 'password',
+                        message: "length must be greater than 3"
+                    }],
+                };
+                }
+           
+            const hashedPassword = await argon2.hash(options.password);
+            let user;
+
+            try{
+            const result = await (em as EntityManager)
+            .createQueryBuilder(User)
+            .getKnexQuery()
+            .insert({
+                username: options.username,
+                password: hashedPassword,
+                created_at: new Date(),
+                updated_at: new Date(),
+            })
+            .returning("*");
+            user = result[0];
+            } catch(err) {
+                if (err.code === "23505"){ // || err.details.includes("already exists")){
+                    return {
+                        errors: [
+                            {
+                                field: 'username',
+                                message: "username already taken"
+                            }
+                        ]
+                    }
+                }
+            }
+            //store user id in session, keeps user logged in
+        req.session!.userId = user.id;
+
+            return {user};
+        
+        }
+
+        @Mutation(()=> UserResponse)
+    async login(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() {em, req}: MyContext
+        ): Promise<UserResponse> {
+            const user = await em.findOneOrFail(User, {username: options.username})
+            if (!user){
+                return {
+                    errors:[
+                    {field: 'username',
+                    message: "that username doesn't exist",
+                 },
+                ],
+                };
+            }
+           
+            const valid = await argon2.verify(user.password, options.password)
+            if (!valid){
+                return{
+                    errors:[
+                        {field: 'password',
+                        message: "incorrect password",
+                    },
+                ],
+            };
+        }
+
+        req.session!.userId = user.id;
+        
+
+            return{ user, 
+            };
+        }
+    }
